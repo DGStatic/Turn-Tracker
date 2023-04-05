e2f39ee90ecd66de26a886cb3f8de5ba8dfea472
Dylan G. Sharp <d00377527@dmail.dixie.edu>
2023-04-04 23:16:14 -0600

    quick fix

--- turn_tracker/pages/api/scenarios/create.js
+++ turn_tracker/pages/api/scenarios/create.js
@@ -3,7 +3,8 @@ import clientPromise from '../../../lib/mongodb'
 
 export default async (req, res) => {
 
-    if (req.method === 'POST') {
+    if (req.method === 'POST') {  
+	res.setHeader('Access-Control-Allow-Origin', 'https://turn-tracker.vercel.app');
         const client = await clientPromise
         let db;
         let scenarios;
@@ -13,9 +14,8 @@ export default async (req, res) => {
         } catch (e) {
             console.error(e);
         }
-	    res.setHeader('Access-Control-Allow-Origin', 'https://turn-tracker.vercel.app')
             res.status(500).json({message: "Database error. Please try again later."})
-        }
+  
         const scenario = req.body
         var result;
         const rows = JSON.parse(JSON.stringify(scenario))
@@ -27,8 +27,7 @@ export default async (req, res) => {
         } catch (e) {
             console.error(e);
             res.status(400).json({message: "Scenario was not able to be stored."});
-        }
-	    res.setHeader('Access-Control-Allow-Origin', 'https://turn-tracker.vercel.app')
+	}
             res.status(201).json({id : result.insertedId})
 
     } else { res.status(404).json({message: "Invalid request."}) }
